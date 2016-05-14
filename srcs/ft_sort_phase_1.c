6f60371d54b9d8ce3972f7dccaf5540da4838592
cledant <cledant@student.42.fr>
2016-05-14 14:54:14 +0200

    normisation de ft_sort_phase_1.c et ft_sort_phase_1_in_b.c

--- srcs/ft_sort_phase_1.c
+++ srcs/ft_sort_phase_1.c
@@ -6,19 +6,75 @@
 /*   By: cledant <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/09 13:18:55 by cledant           #+#    #+#             */
-/*   Updated: 2016/05/13 19:47:35 by cledant          ###   ########.fr       */
+/*   Updated: 2016/05/14 14:13:48 by cledant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int		ft_sort_phase_1(t_stack *a, t_stack *b, t_list **list, int debug)
+static inline int		ft_do_swap(t_stack *a, t_stack *b, t_list **list,
+							int debug)
 {
-	int		val[2];
+	ft_stack_swap(a);
+	if (ft_sort_add_list("sa", 3, list) == -1)
+		return (-1);
+	if (debug == 1)
+		ft_stack_display_both(a, b);
+	return (1);
+}
+
+static inline int		ft_do_first_if(t_stack *a, t_stack *b, t_list **list,
+							int debug)
+{
+	ft_stack_rotate(a);
+	if (ft_sort_add_list("ra", 3, list) == -1)
+		return (-1);
+	if (debug == 1)
+		ft_stack_display_both(a, b);
+	if (ft_sort_phase_1_in_b(a, b, list, debug) == -1)
+		return (-1);
+	return (0);
+}
+
+static inline int		ft_do_else(t_stack *a, t_stack *b, t_list **list,
+							int debug)
+{
+	ft_stack_rotate(a);
+	if (ft_sort_add_list("ra", 3, list) == -1)
+		return (-1);
+	if (debug == 1)
+		ft_stack_display_both(a, b);
+	return (0);
+}
+
+static inline int		ft_do_second_if(t_stack *a, t_stack *b, t_list **list,
+							int debug)
+{
+	if (a->array[a->in - 1] == ft_stack_min_value(a) &&
+			a->array[a->in - 2] > a->array[a->in - 3])
+	{
+		if (ft_do_first_if(a, b, list, debug) == -1)
+			return (-1);
+	}
+	else if (a->array[a->in - 1] < a->array[0] ||
+				a->array[a->in - 1] > a->array[a->in - 2])
+	{
+		if (ft_sort_phase_1_in_b(a, b, list, debug) == -1)
+			return (-1);
+	}
+	else
+	{
+		if (ft_do_else(a, b, list, debug) == -1)
+			return (-1);
+	}
+	return (0);
+}
+
+int						ft_sort_phase_1(t_stack *a, t_stack *b,
+							t_list **list, int debug)
+{
 	int		sort;
 
-	val[0] = ft_stack_min_value(a);
-	val[1] = ft_stack_max_value(a);
 	while (a->in > 2)
 	{
 		if ((sort = ft_is_stack_sort_not_min_top(a)) == -1)
@@ -30,40 +86,10 @@ int		ft_sort_phase_1(t_stack *a, t_stack *b, t_list **list, int debug)
 			if ((sort = ft_sort_phase_1_try_swap(a)) == -1)
 				return (-1);
 			else if (sort == 1)
-			{
-				ft_stack_swap(a);
-				if (ft_sort_add_list("sa", 3, list) == -1)
-					return (-1);
-				if (debug == 1)
-					ft_stack_display_both(a, b);
-				return (1);
-			}
+				return (ft_do_swap(a, b, list, debug));
 		}
-		if (a->array[a->in - 1] == val[0] &&
-				a->array[a->in - 2] > a->array[a->in - 3])
-		{
-			ft_stack_rotate(a);
-			if (ft_sort_add_list("ra", 3, list) == -1)
-				return (-1);
-			if (debug == 1)
-				ft_stack_display_both(a, b);
-			if (ft_sort_phase_1_in_b(a, b, list, debug) == -1)
-				return (-1);
-		}
-		else if (a->array[a->in - 1] < a->array[0] ||
-					a->array[a->in - 1] > a->array[a->in - 2])
-		{
-			if (ft_sort_phase_1_in_b(a, b, list, debug) == -1)
-				return (-1);
-		}
-		else
-		{
-			ft_stack_rotate(a);
-			if (ft_sort_add_list("ra", 3, list) == -1)
-				return (-1);
-			if (debug == 1)
-				ft_stack_display_both(a, b);
-		}
+		if (ft_do_second_if(a, b, list, debug) == -1)
+			return (-1);
 	}
 	return (1);
 }
--- srcs/ft_sort_phase_1_in_b.c
+++ srcs/ft_sort_phase_1_in_b.c
@@ -6,87 +6,90 @@
 /*   By: cledant <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/09 13:52:06 by cledant           #+#    #+#             */
-/*   Updated: 2016/05/13 19:48:50 by cledant          ###   ########.fr       */
+/*   Updated: 2016/05/14 14:51:18 by cledant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int		ft_sort_phase_1_in_b(t_stack *a, t_stack *b, t_list **list, int debug)
+static inline int	ft_do_push(t_stack *a, t_stack *b, t_list **list,
+						int debug)
 {
-	int		val_b[2];
-	int		way;
+	ft_stack_push(b, a);
+	if (ft_sort_add_list("pb", 3, list) == -1)
+		return (-1);
+	if (debug == 1)
+		ft_stack_display_both(a, b);
+	return (1);
+}
 
-	if (b->in == 0 || b->in == 1)
+static inline int	ft_do_rotate_push(t_stack *a, t_stack *b, t_list **list,
+						int debug)
+{
+	ft_stack_rotate(b);
+	if (ft_sort_add_list("rb", 3, list) == -1)
+		return (-1);
+	if (debug == 1)
+		ft_stack_display_both(a, b);
+	ft_stack_push(b, a);
+	if (ft_sort_add_list("pb", 3, list) == -1)
+		return (-1);
+	if (debug == 1)
+		ft_stack_display_both(a, b);
+	return (1);
+}
+
+static inline int	ft_do_b_rotate(int way, t_stack *b, t_list **list)
+{
+	if (way == 1)
 	{
-		ft_stack_push(b, a);
-		if (ft_sort_add_list("pb", 3, list) == -1)
+		ft_stack_rotate(b);
+		if (ft_sort_add_list("rb", 3, list) == -1)
 			return (-1);
-		if (debug == 1)
-			ft_stack_display_both(a, b);
-		return (1);
 	}
 	else
 	{
-		if ((way = ft_sort_r_or_rr_p1b(a, b)) == -1)
+		ft_stack_rev_rotate(b);
+		if (ft_sort_add_list("rrb", 4, list) == -1)
 			return (-1);
-		while (1)
-		{
-			val_b[0] = ft_stack_min_value(b);
-			val_b[1] = ft_stack_max_value(b);
-			if (a->array[a->in - 1] < b->array[b->in - 1]
-					&& a->array[a->in - 1] > b->array[0])
-			{
-				ft_stack_push(b, a);
-				if (ft_sort_add_list("pb", 3, list) == -1)
-					return (-1);
-				if (debug == 1)
-					ft_stack_display_both(a, b);
-				return (1);
-			}
-			else if (a->array[a->in - 1] > val_b[1] &&
-						b->array[b->in - 1] == val_b[1])
-			{
-				ft_stack_rotate(b);
-				if (ft_sort_add_list("rb", 3, list) == -1)
-					return (-1);
-				if (debug == 1)
-					ft_stack_display_both(a, b);
-				ft_stack_push(b, a);
-				if (ft_sort_add_list("pb", 3, list) == -1)
-					return (-1);
-				if (debug == 1)
-					ft_stack_display_both(a, b);
-				return (1);
-			}
-			else if (a->array[a->in - 1] < val_b[0] &&
-						b->array[b->in - 1] == val_b[0])
-			{
-				ft_stack_push(b, a);
-				if (ft_sort_add_list("pb", 3, list) == -1)
-					return (-1);
-				if (debug == 1)
-					ft_stack_display_both(a, b);
-				return (1);
-			}
-			else
-			{
-				if (way == 1)
-				{
-					ft_stack_rotate(b);
-					if (ft_sort_add_list("rb", 3, list) == -1)
-						return (-1);
-				}
-				else
-				{
-					ft_stack_rev_rotate(b);
-					if (ft_sort_add_list("rrb", 4, list) == -1)
-						return (-1);
-				}
-				if (debug == 1)
-					ft_stack_display_both(a, b);
-			}
-		}
-		return (1);
 	}
+	return (0);
+}
+
+static inline int	ft_do_first_else(t_stack *a, t_stack *b, t_list **list,
+						int debug)
+{
+	int		way;
+
+	if ((way = ft_sort_r_or_rr_p1b(a, b)) == -1)
+		return (-1);
+	while (1)
+	{
+		if (a->array[a->in - 1] < b->array[b->in - 1]
+				&& a->array[a->in - 1] > b->array[0])
+			return (ft_do_push(a, b, list, debug));
+		else if (a->array[a->in - 1] > ft_stack_max_value(b)
+				&& b->array[b->in - 1] == ft_stack_max_value(b))
+			return (ft_do_rotate_push(a, b, list, debug));
+		else if (a->array[a->in - 1] < ft_stack_min_value(b)
+				&& b->array[b->in - 1] == ft_stack_min_value(b))
+			return (ft_do_push(a, b, list, debug));
+		else
+		{
+			if (ft_do_b_rotate(way, b, list) == -1)
+				return (-1);
+			if (debug == 1)
+				ft_stack_display_both(a, b);
+		}
+	}
+	return (1);
+}
+
+int					ft_sort_phase_1_in_b(t_stack *a, t_stack *b,
+						t_list **list, int debug)
+{
+	if (b->in == 0 || b->in == 1)
+		return (ft_do_push(a, b, list, debug));
+	else
+		return (ft_do_first_else(a, b, list, debug));
 }
